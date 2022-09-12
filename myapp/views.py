540f0e2386623376b3b976f642121341205bac67
sarorsaad <sarorsaad@gmail.com>
2022-09-12 19:52:39 +0300

    create view

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import posts
+
+
+def all_posts(request):
+    posts=Post.objects.all()
+    return render(request,'posts.html',{'saror':posts})
